dadd5f62f90a714e535448cefd43cb9210851afb
Mikhail Kormyshov <kormyshov@gmail.com>
2014-01-13 00:16:42 +0800

    Add msb

---
+++ msb.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <cstring>
+#include <memory.h>
+#include <cmath>
+#include <iomanip>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <list>
+#include <bitset>
+#include <algorithm>
+
+#define ABS(a) ((a)<0?(-(a)):(a))
+#define SIGN(a) (((a)>0)-((a)<0))
+#define SQR(a) ((a)*(a))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a,b) ((a)<(b)?(a):(b))
+
+#define PI (3.1415926535897932384626433832795)
+#define INF (2147483647)
+#define LLINF (9223372036854775807LL)
+#define INF2 (1073741823)
+#define EPS (0.00000001)
+
+#define MOD (1000000007)
+
+#define y1 stupid_cmath
+#define y0 stupid_cmath_too
+
+using namespace std;
+
+typedef long long LL;
+template<typename T1,typename T2> ostream& operator<<(ostream &O,pair<T1,T2> &t) {return O<<"("<<t.first<<","<<t.second<<")";}
+template<typename T> ostream& operator<<(ostream &O,vector<T> &t){for(int _=0;_<(int)t.size();++_)O<<t[_]<<" ";return O; }
+void dout(){cout<<endl;} template<typename Head, typename... Tail> void dout(Head H, Tail... T){cout<<H<<" "; dout(T...);}
+
+
+
+
+int msb(unsigned x) {
+	union { double a; int b[2]; };
+	a = x;
+	return (b[1] >> 20) - 1023;
+}
+
+
+int main()
+{
+	//ios_base::sync_with_stdio(0);
+
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	return 0;
+}
+
+int cpp4cf_main()
+{
+	freopen("msb.cpp","r",stdin);
+
+	char s[99];
+	bool f;
+
+	while(true) {
+		cin>>s;
+		if(cin.eof()) break;
+		if(strstr(s,"/*")) {
+			cin>>s;
+			if(strstr(s,"Test")) {
+				cin>>s;
+				if(strstr(s,"on")) {
+					cout<<"Output: ";
+					cpp4cf_main();
+					cout<<"\nAnswer: ";
+					f = false;
+					while(true) {
+						cin>>s;
+						if(strstr(s,"*/")) break;
+						if(strstr(s,"//")) {
+							if(f) cout<<endl;
+							else f = true;
+						}else cout<<s<<" ";
+					}
+					cout<<"\n\n";
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
